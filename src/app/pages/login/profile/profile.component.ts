424c629d233b4c2b77094bb527b37018dd09dc41
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-10-12 07:08:03 -0300

    add profile

--- src/app/pages/login/profile/profile.component.ts
+++ src/app/pages/login/profile/profile.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormsModule , ReactiveFormsModule, Validators} from '@angular/forms';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';  
+import { InputTextModule } from 'primeng/inputtext';  
+import { CardModule } from 'primeng/card';  
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule,FileUploadModule,ButtonModule,InputTextModule,CardModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
 
+
+  updateUserProfile(){
+
+  }
+  onPhotoUpload(event: any){
+
+  }
 }
