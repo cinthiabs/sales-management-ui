b4beb57f27b6c1174df1fecfbcc0ed4de9806297
cinthiabs <cinthiabarbosa8d@outlook.com>
2024-10-12 07:27:05 -0300

    add dropdown

--- src/app/pages/login/profile/profile.component.ts
+++ src/app/pages/login/profile/profile.component.ts
@@ -4,16 +4,27 @@ import { FileUploadModule } from 'primeng/fileupload';
 import { ButtonModule } from 'primeng/button';  
 import { InputTextModule } from 'primeng/inputtext';  
 import { CardModule } from 'primeng/card';  
+import { DropdownModule } from 'primeng/dropdown';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,FileUploadModule,ButtonModule,InputTextModule,CardModule],
+  imports: [FormsModule,ReactiveFormsModule,FileUploadModule,ButtonModule,InputTextModule,CardModule, DropdownModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  userTypes = [
+    {name: 'Adm', value: true},
+    {name: 'Usuario', value: false}
+  ]
 
-
+  createForm: FormGroup = this.fb.group({
+    userTypes: [''],
+    createDate: [{value: '', disabled: true}],
+    editDate: [{value: '', disabled: true}] 
+  })
+  constructor(
+    private fb: FormBuilder){}
   updateUserProfile(){
 
   }
